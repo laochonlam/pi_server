f5316497c467025290d75eaa5795d6619a677443
laochanlam <laochanlam@gmail.com>
2018-12-06 15:32:20 +0000

    add GPIO

--- routes/functions.js
+++ routes/functions.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var rpio = require('rpio');
+
 var router = express.Router();
 
 /* GET functions listing. */
 router.get('/', function(req, res, next) {
+  rpio.open(36, rpio.OUTPUT, rpio.HIGH);
+  console.log('turn up pin 36');
+  while(1) {
+	  console.log(rpio.read(36));
+  }
   res.send('functioning');
 });
 
